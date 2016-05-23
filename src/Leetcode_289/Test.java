5e0041d74e098d565fb5f9a946bc1d2ee32e0219
Nelofie <466678591@qq.com>
2016-05-24 01:56:50 +0800

    289
    补5.21的

---
+++ src/Leetcode_289/Test.java
@@ -0,0 +1,10 @@
+package Leetcode_289;
+
+public class Test {
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.addDigits(38));
+		System.out.println(sol.addDigits(648));
+		System.out.println(sol.addDigits(65535));
+	}
+}
