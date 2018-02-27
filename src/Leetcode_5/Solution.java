815edf02657b77da35436ff8dad5531c709aacf9
Nelofie <466678591@qq.com>
2018-02-27 17:54:28 +0800

    manacher

--- src/Leetcode_5/Solution.java
+++ src/Leetcode_5/Solution.java
@@ -2,7 +2,7 @@ package Leetcode_5;
 
 /**
  * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
- *
+ * <p>
  * Example:
  * Input: "babad"
  * Output: "bab"
@@ -26,7 +26,7 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             longest = searchSides(i, i, s, longest);
-            longest = searchSides(i, i+1, s, longest);
+            longest = searchSides(i, i + 1, s, longest);
         }
         return longest;
     }
@@ -53,15 +53,65 @@ class Solution {
      * https://segmentfault.com/a/1190000003914228
      */
     public String manacher(String s) {
-        //todo
-        return null;
+        String sNew = s.replace("", "#");
+        int[] rl = new int[sNew.length()];
+        int resL = 0;
+        int resR = 0;
+
+        int maxRight = 0;
+        int pos = 0;
+        for (int i = 0; i < sNew.length(); i++) {
+            if (i < maxRight) {
+                rl[i] = min(rl[2 * pos - i], maxRight - i);
+            } else {
+                rl[i] = 1;
+            }
+            while (i - rl[i] >= 0 && i + rl[i] < sNew.length() && sNew.charAt(i - rl[i]) == sNew.charAt(i + rl[i])) {
+                rl[i]++;
+            }
+            if (i + rl[i] - 1 > maxRight) {
+                maxRight = i + rl[i] - 1;
+                pos = i;
+            }
+            if (resR - resL + 1 < (maxRight - pos) * 2 + 1) {
+                resL = 2 * pos - maxRight;
+                resR = maxRight;
+            }
+        }
+        return sNew.substring(resL, resR).replace("#", "");
+    }
+
+    private int min(int a ,int b) {
+        return a > b ? b : a;
+    }
+
+
+    /**
+     * 动态规划
+     */
+    public int dynamicDivide(String s) {
+        int n = s.length();
+        boolean[][] pal = new boolean[n][n];
+        //pal[i][j] 表示s[i...j]是否是回文串
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {  // i作为终点
+            int j = i;    //j作为起点
+            while (j >= 0) {
+                if (s.charAt(j) == s.charAt(i) && (i - j < 2 || pal[j + 1][i - 1])) {
+                    pal[j][i] = true;
+                    maxLen = Math.max(maxLen, i - j + 1);
+                }
+                j--;
+            }
+        }
+        return maxLen;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.longestPalindrome("a"));
-        System.out.println(s.longestPalindrome("cbbd"));
-        System.out.println(s.longestPalindrome("abcda"));
-        System.out.println(s.longestPalindrome("aaabaaaa"));
+        System.out.println(s.manacher("abbahopxpo"));
+        System.out.println(s.manacher("cbbd"));
+        System.out.println(s.manacher("abcda"));
+        System.out.println(s.manacher("aaabaaaa"));
     }
 }
